43e88f3d61730a447f2f36f6ba5705fdd4582f09
rajeshsaharan <rajeshsharan1607@gmail.com>
2023-01-05 08:21:36 +0530

    racket

---
+++ wk22023-smlRacket/racket/local efficency/basic.rkt
@@ -0,0 +1,34 @@
+; return maximum number from list
+
+(check-expect (bad_max empty) 0)
+(check-expect (bad_max (list 1)) 1)
+(check-expect (bad_max (list 1 2)) 2)
+(check-expect (bad_max (list 1 2 3)) 3)
+(check-expect (bad_max (list 1 2 3 4 5)) 5)
+
+
+
+
+(define (bad_max lon) (cond [(empty? lon) 0] ; consider what base case produce rest of recursion produce it that term
+                            [(empty? (rest lon)) (first lon)]
+                            [else (if (> (first lon) (bad_max (rest lon))) (first lon) (bad_max (rest lon)))]
+                            ))
+
+
+(bad_max (list 1 2 3 4 ))
+
+
+
+
+;;here is a sort the list function
+
+
+(define (sort2 lon) (cond [(empty? lon) lon]
+                          [(empty? (rest lon)) lon]
+                          [else (compare (first lon) (sort2 (rest lon)))]))
+
+(define (compare n lon) (cond [(empty? lon) (cons n empty)]
+                              [else (if (> n (first lon)) (cons (first lon) (compare n (rest lon))) (cons n lon))]))
+
+
+(sort2 (list 4 5 2 1))
